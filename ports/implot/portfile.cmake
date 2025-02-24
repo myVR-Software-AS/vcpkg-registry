e2c6186c185be9affadd0c605a3965343d3f56b8
Artem Sidyakin <artem@myvr-software.com>
2025-02-24 12:50:13 +0100

    [implot] new port

---
+++ ports/implot/portfile.cmake
@@ -0,0 +1,41 @@
+# does not export symbols for making a DLL
+if(VCPKG_TARGET_IS_WINDOWS)
+    vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+endif()
+
+vcpkg_from_git(
+    OUT_SOURCE_PATH SOURCE_PATH
+    URL git@github.com:epezent/implot.git
+    REF 18c72431f8265e2b0b5378a3a73d8a883b2175ff
+    PATCHES
+        001-dear-imgui-headers-from-package.patch
+)
+
+file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt"   DESTINATION "${SOURCE_PATH}")
+file(COPY "${CMAKE_CURRENT_LIST_DIR}/Config.cmake.in"  DESTINATION "${SOURCE_PATH}")
+file(COPY
+    "${CURRENT_HOST_INSTALLED_DIR}/share/myvr-vcpkg-cmake/common/Installing.cmake"
+    DESTINATION "${SOURCE_PATH}"
+)
+
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES
+        with-internal WITH_INTERNAL
+)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        ${FEATURE_OPTIONS}
+)
+
+vcpkg_cmake_install()
+
+vcpkg_cmake_config_fixup(
+    PACKAGE_NAME "ImPlot"
+    CONFIG_PATH "share/ImPlot"
+)
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
