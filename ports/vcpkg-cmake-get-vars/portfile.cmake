9345693711e03f911d7df08659cd3811a88ab510
Artem Sidyakin <artem@myvr-software.com>
2025-02-24 14:09:36 +0100

    [vcpkg-cmake-get-vars] new port

---
+++ ports/vcpkg-cmake-get-vars/portfile.cmake
@@ -0,0 +1,14 @@
+if(VCPKG_CROSSCOMPILING)
+    # should be FATAL_ERROR
+    message(WARNING "${PORT} is a host-only port, mark it as a host dependency in your ports")
+endif()
+
+file(INSTALL
+    "${CMAKE_CURRENT_LIST_DIR}/vcpkg_cmake_get_vars.cmake"
+    "${CMAKE_CURRENT_LIST_DIR}/cmake_get_vars"
+    "${CMAKE_CURRENT_LIST_DIR}/vcpkg-port-config.cmake"
+    DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+
+vcpkg_install_copyright(FILE_LIST "${VCPKG_ROOT_DIR}/LICENSE.txt")
+
+set(VCPKG_POLICY_CMAKE_HELPER_PORT enabled)
